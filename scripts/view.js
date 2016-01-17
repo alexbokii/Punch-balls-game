3bdfe448a403fcc9d9f52d83c4c89d033c807c8d
Alex Bokii <vanilinka33@gmail.com>
2016-01-17 17:10:20 +0000

    Added base functions
    Added functions to create balls and append them to targets, base moving
    functions

--- scripts/view.js
+++ scripts/view.js
@@ -1,24 +1,49 @@
-// Random init
-
-var balls = ['red', 'yellow', 'blue', 'green'];
-var selectedBalls = [];
-
-for(var i = 0; i < 3; i++) {
-    var initBall = balls[Math.floor(Math.random()*balls.length)];
-    selectedBalls.push(initBall);
-}
-
+// init
 var squares = $('.square');
-var selectedSquares = [];
-for(var i = 0; i < 3; i++) {
-    var square = squares[Math.floor(Math.random()*squares.length)];
-    selectedSquares.push(square);
+addMoreBalls(5);
+
+for(var i = 0; i < squares.length; i++) {
+    $(squares[i]).attr('id', 'square' + (i + 1));
 }
 
-$.each(selectedSquares, function(i, item){
-    $(item).append("<div class='ball ball-" + selectedBalls[i] + "'></div>");
-})
 
-// Draggable
+// base functions
 
-$('.square').attr('draggable', 'true');
+// add balls
+function addMoreBalls(num) {
+    var balls = ['red', 'yellow', 'blue', 'green'];
+
+    for(var i = 0; i < num; i++) {
+        var newball = balls[Math.floor(Math.random()*balls.length)];
+        do {
+            var square = squares[Math.floor(Math.random()*squares.length)];
+        }
+        while (!targetIsEmpty(square));
+        $(square).append("<div class='ball ball-" + newball + "'></div>");
+    }
+}
+
+// move balls
+var ball;
+var target;
+
+$('.square').on('click', function() {
+    if(ball == undefined) {
+        ball = $(this).find('.ball');
+    }
+    else {
+        $(this).append(ball);
+        addMoreBalls(2);
+        clearCurrentTurn();
+    }
+});
+
+function clearCurrentTurn() {
+    ball = undefined;
+    target = undefined;
+}
+
+// check if target is empty
+function targetIsEmpty(target) {
+    return $(target).is(':empty');
+}
